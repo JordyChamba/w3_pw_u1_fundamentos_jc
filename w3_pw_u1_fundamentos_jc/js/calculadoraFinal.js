900bb623a67b076e6cfc63056c19a6c6a2471df8
Jordy Chamba <jordy_chamba@hotmail.com>
2025-11-07 19:40:49 -0500

    Maquetacion-taller8

---
+++ w3_pw_u1_fundamentos_jc/js/calculadoraFinal.js
@@ -0,0 +1,18 @@
+let num1=0;
+let num2=0;
+
+function sumar(num1,num2){
+    let resultado = num1 + num2;
+    return ;
+}
+function restar(){
+function multiplicar(){
+}
+function dividir(){
+}   
+function porcentaje(){
+}
+function mostrarDisplay(valor){
+    let elemento = document.getElementById("id_display");
+    elemento.innerText = elemento.innerText + valor;
+}
